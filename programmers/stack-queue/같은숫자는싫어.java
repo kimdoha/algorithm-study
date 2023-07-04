949b479c52047fb2c5a8ee2cbbce118952c0eab1
김도하 <62235737+kimdoha@users.noreply.github.com>
2023-07-04 02:38:06 +0000

    feat: 스택큐_같은숫자는싫어(Lv1)

---
+++ programmers/stack-queue/같은숫자는싫어.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+public class 같은숫자는싫어 {
+
+  public int[] solution(int []arr) {
+    Stack<Integer> stack = new Stack<>();
+
+    stack.add(arr[0]);
+
+    for(int i = 1; i < arr.length; i++) {
+      if(stack.peek() == arr[i]) continue;
+      stack.add(arr[i]);
+    }
+
+    return stack.stream().mapToInt(Integer::intValue).toArray();
+  }
+  
+}
