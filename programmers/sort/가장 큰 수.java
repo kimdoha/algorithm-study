e692e0f6b6273cb3ab76e017a73e05b19eb49674
김도하 <62235737+kimdoha@users.noreply.github.com>
2023-07-06 07:39:42 +0000

    feat: 정렬_가장 큰 수(Lv2)

---
+++ programmers/sort/가장 큰 수.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+class Solution {
+
+  static class Elem implements Comparable<Elem> {
+    String num;
+    @Override
+    public int compareTo(Elem o) {
+      return (o.num+num).compareTo(num+o.num);
+    }
+  }
+
+
+
+  public String solution(int[] numbers) {
+    String answer = "";
+    Elem[] elms = new Elem[numbers.length];
+
+    int zero_count = 0;
+    for(int i = 0; i < numbers.length; i++) {
+      elms[i] = new Elem();
+      elms[i].num = String.valueOf(numbers[i]);
+      if(numbers[i] == 0) zero_count++;
+    }
+
+    if(zero_count == numbers.length) return "0";
+
+    Arrays.sort(elms);
+    for(Elem elem : elms) {
+      answer += elem.num;
+    }
+    return answer;
+  }
+}
