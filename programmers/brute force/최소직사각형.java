020139ce444a140caaa2450e1ed667e19ddd7b67
김도하 <62235737+kimdoha@users.noreply.github.com>
2023-07-17 11:11:50 +0000

    feat: 완전탐색_최소직사각형 (Lv.1)

---
+++ programmers/brute force/최소직사각형.java
@@ -0,0 +1,13 @@
+class Solution {
+    
+    public int solution(int[][] sizes) {
+       int width = 0, height = 0;
+   
+       for(int[] size : sizes) {
+         width = Math.max(width, Math.max(size[0], size[1]));
+         height = Math.max(height, Math.min(size[0], size[1]));
+       }
+   
+       return width * height;
+    }
+}
