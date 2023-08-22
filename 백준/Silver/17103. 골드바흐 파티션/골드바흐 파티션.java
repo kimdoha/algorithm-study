fc06bbabc1d032aea5fa1c9286501079cb04b221
kimdoha <62235737+kimdoha@users.noreply.github.com>
2023-08-22 16:39:25 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 488 ms, Memory: 15348 KB -BaekjoonHub

---
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+  static boolean[] primeNumbers = new boolean[1000001];
+
+  static class Solution {
+
+    int getNumberOfPartition(int num) {
+      int answer = 0;
+      
+      for(int i = 1; i <= num / 2; i++) {
+        if(primeNumbers[i] && primeNumbers[num - i]) answer++;
+      }
+
+      return answer;
+    }
+  }
+
+  static boolean isPrimeNumber(int num) {
+    for (int decimal = 2; decimal <= Math.sqrt(num); decimal++ ) {
+      if (num % decimal == 0) return false;
+    }
+
+    return true;
+  }
+
+  static void init() {
+
+    for(int find = 2; find <= 1000000; find++) {
+      if(isPrimeNumber(find)) primeNumbers[find] = true;
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+    int T = Integer.parseInt(br.readLine());
+    init();
+
+    Solution solution = new Solution();
+    while (T-- > 0) {
+      int num = Integer.parseInt(br.readLine());
+      System.out.println(solution.getNumberOfPartition(num));
+    }
+  }
+}
+
