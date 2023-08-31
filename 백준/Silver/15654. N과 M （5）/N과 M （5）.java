c03def81368ed83b666b5f80634856f8d4e7d3e2
kimdoha <62235737+kimdoha@users.noreply.github.com>
2023-08-31 19:58:56 +0900

    [Silver III] Title: N과 M (5), Time: 364 ms, Memory: 28336 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+  static int N, M;
+  static int[] selected, numbers;
+  static boolean[] visited;
+
+  static StringBuilder sb = new StringBuilder();
+
+  static void rec_func(int k) {
+    if(k == M + 1) {
+      for(int i = 1; i <= M; i++) sb.append(selected[i]).append(" ");
+      sb.append("\n");
+    } else {
+      for(int cand = 1; cand <= N; cand++) {
+        if(!visited[cand]) {
+          selected[k] = numbers[cand];
+          visited[cand] = true;
+          rec_func(k + 1);
+          selected[k] = 0;
+          visited[cand] = false;
+        }
+      }
+    }
+  }
+    
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    numbers = new int[N + 1];
+    selected = new int[M + 1];
+    visited = new boolean[N + 1];
+
+    st = new StringTokenizer(br.readLine());
+    for(int i = 1; i <= N; i++) {
+      numbers[i] = Integer.parseInt(st.nextToken());
+    }
+    Arrays.sort(numbers);
+
+    rec_func(1);
+    System.out.println(sb.toString());
+  }
+}
