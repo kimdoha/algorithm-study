c17c8497f39cc2bd17b606392d452e6366daff6f
kimdoha <62235737+kimdoha@users.noreply.github.com>
2023-08-31 19:17:52 +0900

    [Silver III] Title: N과 M (1), Time: 268 ms, Memory: 22272 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static int N, M;
+  static int[] selected;
+  static boolean[] visited;
+  static StringBuilder sb = new StringBuilder();
+
+  static void rec_func(int k) {
+    if(k == M + 1) {
+      for(int i = 1; i <= M; i++) {
+        sb.append(selected[i]).append(" ");
+      }
+      sb.append("\n");
+    } else {
+      for(int cand = 1; cand <= N; cand++) {
+        if(!visited[cand]) {
+          selected[k] = cand;
+          visited[cand] = true;
+          rec_func( k + 1 );
+          selected[k] = 0;
+          visited[cand] = false;
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    selected = new int[N + 1];
+    visited = new boolean[N + 1];
+
+    rec_func(1);
+    System.out.println(sb.toString());
+  }
+}
