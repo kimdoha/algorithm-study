b8c1448c39cc75eedf328f48376d25e1a48ca079
kimdoha <62235737+kimdoha@users.noreply.github.com>
2023-09-04 19:44:38 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 144 ms, Memory: 15228 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -3,45 +3,49 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-
 public class Main {
-    
-    static int N = 9, M = 7;
-    static int[] dwarfs, selected;
-    static boolean[] visited;
-    static StringBuilder sb = new StringBuilder();
-    
-    static void dfs(int k, int sum) {
-        if(k == M + 1) {
-            if(sum == 100 && sb.toString().length() == 0) {
-                for(int i = 1; i <= M; i++) sb.append(selected[i]).append("\n");
-            }
-        } else {
-            for(int cand = 1; cand <= N; cand++) {
-                if(!visited[cand]) {
-                    selected[k] = dwarfs[cand];
-                    visited[cand] = true;
-                    dfs(k + 1, sum + dwarfs[cand]);
-                    selected[k] = 0;
-                    visited[cand] = false;
-                }
-            }
-        }
-    }
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        dwarfs = new int[N + 1];
-        visited = new boolean[N + 1];
-        selected = new int[M + 1];
-        
-        for(int i = 1; i <= N; i++) {
-          dwarfs[i] = Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(dwarfs);
+  static int N = 9, M = 7;
+  static int[] dwarfs, selected, result;
+  static boolean[] visited;
+  static StringBuilder sb = new StringBuilder();
 
-        dfs(1, 0);
-        System.out.println(sb.toString());
+  static void dfs(int k, int sum) {
+    if(k == M + 1) {
+      if(sum == 100) {
+        for(int i = 1; i <= M; i++) result[i] = selected[i];
+      return;
+      }
+
+    } else {
+      for(int cand = 1; cand <= N; cand++) {
+        if(!visited[cand]) {
+          selected[k] = dwarfs[cand];
+          visited[cand] = true;
+          dfs(k + 1, sum + dwarfs[cand]);
+          selected[k] = 0;
+          visited[cand] = false;
+        }
+      }
     }
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    dwarfs = new int[N + 1];
+    visited = new boolean[N + 1];
+    selected = new int[M + 1];
+    result = new int[M + 1];
+
+    for(int i = 1; i <= N; i++) {
+      dwarfs[i] = Integer.parseInt(br.readLine());
+    }
+    Arrays.sort(dwarfs);
+
+    dfs(1, 0);
+
+    Arrays.sort(result);
+    for(int i = 1; i <= M; i++) sb.append(result[i]).append("\n");
+    System.out.println(sb.toString());
+  }
 }
