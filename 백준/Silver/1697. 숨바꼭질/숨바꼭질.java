791c586747b87e714b73106ebb4a9bb894d25b4e
kimdoha <62235737+kimdoha@users.noreply.github.com>
2023-09-07 12:21:13 +0900

    [Silver I] Title: 숨바꼭질, Time: 168 ms, Memory: 23340 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    static int N, K;
+    static int minTime = 0;
+    static boolean[] visited = new boolean[100001];
+    
+    static class Location {
+        int x, time;
+        
+        public Location(int _x, int _time) {
+            this.x = _x;
+            this.time = _time;
+        }
+    }
+
+    static void bfs(Location start) {
+        Queue<Location> que = new LinkedList<>();
+        que.add(start);
+        visited[start.x] = true;
+        
+        while(!que.isEmpty()) {
+            Location cur = que.poll();
+            
+            if(cur.x == K) {
+                minTime = cur.time;
+                break;
+            }
+
+            int[] moveList = {cur.x - 1, cur.x + 1, cur.x * 2};
+            
+            for(int move : moveList) {
+                if(move < 0 || move > 100000) continue;
+                
+                if(!visited[move]) {
+                    que.add(new Location(move, cur.time + 1));
+                    visited[move] = true;
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        
+        bfs(new Location(N, 0));
+        System.out.println(minTime);
+    }
+}
