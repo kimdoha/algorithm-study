6f268f33ecaaab2d609d7bd58dbe2f84195622d3
kimdoha <62235737+kimdoha@users.noreply.github.com>
2023-09-15 20:40:26 +0900

    Update 부분수열의 합.java

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -2,33 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, S;
-    static int[] numbers;
-    static int expression = 0;
- 
-    static void dfs(int k, int sum) {
-        if(k == N) {
-            if(sum == S) expression++;
-        } else {
-            dfs(k + 1, sum + numbers[k + 1]);
-            dfs(k + 1, sum);
-        }
+  static int n, target;
+  static int[] numbers;
+  static int expression = 0;
+
+  static void dfs(int k, int sum) {
+    if(k == n) {
+      if(sum == target) expression++;
+    } else {
+      dfs(k + 1, sum + numbers[k]); 
+      dfs(k + 1, sum);
     }
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        N = Integer.parseInt(st.nextToken());
-        S = Integer.parseInt(st.nextToken());
-        numbers = new int[N + 1];
-        
-        st = new StringTokenizer(br.readLine());
-        for(int i = 1; i <= N; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        for(int i = 1; i <= N; i++) dfs(i, numbers[i]);
-        System.out.println(expression);
-    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    n = Integer.parseInt(st.nextToken());
+    target = Integer.parseInt(st.nextToken());
+
+    numbers = new int[n];
+
+    st = new StringTokenizer(br.readLine());
+    for(int i = 0; i < n; i++) numbers[i] = Integer.parseInt(st.nextToken());;
+
+    dfs(0, 0);
+      
+    if(target == 0) expression = expression - 1; // 공집합 제외
+    System.out.println(expression);
+  }
 }
