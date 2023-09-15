a26e263a291813034672510b20fd378a6d0759a3
kimdoha <62235737+kimdoha@users.noreply.github.com>
2023-09-15 21:00:04 +0900

    [Silver III] Title: 퇴사, Time: 128 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -3,43 +3,42 @@ import java.util.*;
 
 
 public class Main {
-    static int N, max_cost = 0;;
-    static Consult[] consults;
-    
-    static class Consult {
-        int day;
-        int cost;
-        
-        public Consult(int _day, int _cost) {
-            this.day = _day;
-            this.cost = _cost;
-        }
-    }
-    
-    static void dfs(int k, int sum) {
-        if(k == N + 1) {
-            max_cost = Math.max(max_cost, sum);
-        } else {
-            int next = k + consults[k].day;
-            if(next <= N + 1) dfs(next, sum + consults[k].cost);
-            dfs(k + 1, sum);
-        }
-    }
-    
+  static int N, max_cost = 0;;
+  static Consult[] consults;
 
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        N = Integer.parseInt(br.readLine());
-        consults = new Consult[N + 1];
-        
-        for(int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            consults[i] = new Consult(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-        }
-        
-        for(int i = 1; i <= N; i++) dfs(i, 0);
-        System.out.println(max_cost);
+  static class Consult {
+    int day;
+    int cost;
+
+    public Consult(int _day, int _cost) {
+      this.day = _day;
+      this.cost = _cost;
     }
+  }
+
+  static void dfs(int k, int sum) {
+    if(k == N) {
+      max_cost = Math.max(max_cost, sum);
+    } else {
+      int next = k + consults[k].day;
+      if(next <= N) dfs(next, sum + consults[k].cost);
+      dfs(k + 1, sum);
+    }
+  }
+
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    N = Integer.parseInt(br.readLine());
+    consults = new Consult[N + 1];
+
+    for(int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      consults[i] = new Consult(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+    }
+
+    dfs(0, 0);
+    System.out.println(max_cost);
+  }
 }
