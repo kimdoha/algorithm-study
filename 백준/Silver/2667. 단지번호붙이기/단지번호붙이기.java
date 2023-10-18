40865248e655c1a2b4fd0fe8e82f7cd53f20869a
kimdoha <62235737+kimdoha@users.noreply.github.com>
2023-10-18 22:03:30 +0900

    [Silver I] Title: 단지번호붙이기, Time: 148 ms, Memory: 14772 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,48 +2,70 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, count, block = 0;
-    static List<Integer> apartmentInfo = new ArrayList<>();
+    static int N, apartmentCnt = 0;
+    static ArrayList<Integer> apartmentInfo = new ArrayList<>();
     static int[][] map;
     static boolean[][] visited;
-    static int[][] dxdy = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    static int[][] dxdy = new int[][]{
+        {-1, 0},
+        {1, 0},
+        {0, -1},
+        {0, 1}
+    };
     
-    static void dfs(int x, int y) {
-        if(x < 0 || x >= N || y < 0 || y >= N) return; // 지도에서 벗어난 경우
-        if(map[x][y] == 0 || visited[x][y]) return; // 방문했거나 집이 없는 곳
+    static StringBuilder sb = new StringBuilder();
+    
+    static class Location {
+        int x, y;
         
-        map[x][y] = 0;
-        visited[x][y] = true;
-        count++;
+        public Location(int _x, int _y) {
+            x = _x;
+            y = _y;
+        }
+    }
+    
+    static void dfs(Location loc) {
+        visited[loc.x][loc.y] = true;
+        apartmentCnt++;
         
         for(int i = 0; i < 4; i++) {
-            dfs(x + dxdy[i][0], y + dxdy[i][1]);
+            int tx = loc.x + dxdy[i][0], ty = loc.y + dxdy[i][1];
+            
+            if(tx < 0 || tx >= N || ty < 0 || ty >= N) continue;
+            if(visited[tx][ty]) continue;
+            if(map[tx][ty] == 0) continue;
+            
+            dfs(new Location(tx, ty));
         }
     }
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        visited = new boolean[N][N];
         
+        map = new int[N][N];
         for(int i = 0; i < N; i++) {
             map[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
         }
         
-        for(int x = 0; x < N; x++) {
-            for(int y = 0; y < N; y++) {
-                if(map[x][y] == 1) {
-                    count = 0; // 단지 내 아파트 수
-                    dfs(x, y);
-                    apartmentInfo.add(count);
-                    block++;
+        visited = new boolean[N][N];
+        
+        int count = 0;
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(map[i][j] == 1 && !visited[i][j]) {
+                    apartmentCnt = 0; // 각 단지내 집의 수
+                    dfs(new Location(i, j));
+                    apartmentInfo.add(apartmentCnt);
+                    count++; // 총 단지수
                 }
             }
         }
         
-        System.out.println(block);
         Collections.sort(apartmentInfo);
-        apartmentInfo.stream().forEach(info -> System.out.println(info));
+        sb.append(count).append("\n");
+        apartmentInfo.forEach(info -> sb.append(info).append("\n"));
+        
+        System.out.println(sb.toString());
     }
 }
